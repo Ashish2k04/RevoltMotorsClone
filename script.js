74afeb30eb2c5d646a4b41d5dcb92f89bde3caf3
Ashish Tiwari <iamashishtiwari203@gmail.com>
2025-10-26 21:54:09 +0530

    moved-1

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ let flag = 0;
 document.getElementById("dealer").onclick = function () {
 
     if (flag == 0) {
-        document.getElementById("dff").style.display = "none";
+        document.getElementById("dealers").style.display = "block";
         flag = 1;
     }
     else {
-        document.getElementById("dff").style.display = "block";
+        document.getElementById("dealers").style.display = "none";
+        flag = 0;
+    }
+
+};
+
+let flag2 = 0;
+
+document.getElementById("Support").onclick = function () {
+
+    if (flag == 0) {
+        document.getElementById("spp").style.display = "block";
+        flag = 1;
+    }
+    else {
+        document.getElementById("spp").style.display = "none";
         flag = 0;
     }
 
